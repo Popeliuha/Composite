265fdcf35362b6d03d9c5e871afaa9e153683cda
Suri Miller <popelyshkon25@gmail.com>
2019-03-28 22:39:38 +0200

    Add files via upload

---
+++ Educator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Educator
+    {
+        private string name;
+        private string post;
+        private string groupNumber;
+        public List<Educator> Subordinates { get; set; }
+
+        public Educator(string name, string post, string groupNumber)
+        {
+            this.name = name;
+            this.post = post;
+            this.groupNumber = groupNumber;
+            Subordinates = new List<Educator>();
+        }
+
+        public void AddToSubordinates(Educator educator)
+        {
+            Subordinates.Add(educator);
+        }
+
+        public void RemoveFromSubordinates(Educator educator)
+        {
+            Subordinates.Remove(educator);
+        }
+
+        public string ShowInfo()
+        {
+            return $"Educator's name is {name}, post is {post} and group number is {groupNumber}.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        public static void ShowEducatorsInfo(Educator e)
+        {
+            Console.WriteLine($"HeadMaster: {e.ShowInfo()}");
+            foreach(var sub in e.Subordinates)
+            {
+                Console.WriteLine(sub.ShowInfo()); 
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Educator kinderGartenHeadMaster = new Educator("Ivan", "Head of kindergarten", "1");
+            Educator schoolHeadMaster = new Educator("Andrii", "Head of school", "2");
+            Educator universityDecan = new Educator("Pavlo", "Decan of university", "3");
+
+            Educator educator1 = new Educator("Maks", "Teacher", "4");
+            Educator educator2 = new Educator("Petro", "Teacher", "5");
+            Educator educator3 = new Educator("Vasyl", "Teacher", "6");
+            Educator educator4 = new Educator("Vova", "Teacher", "7");
+            Educator educator5 = new Educator("Mark", "Teacher", "8");
+            Educator educator6 = new Educator("Serhii", "Teacher", "9");
+
+            kinderGartenHeadMaster.AddToSubordinates(educator1);
+            kinderGartenHeadMaster.AddToSubordinates(educator2);
+
+            schoolHeadMaster.AddToSubordinates(educator3);
+            schoolHeadMaster.AddToSubordinates(educator4);
+
+            universityDecan.AddToSubordinates(educator5);
+            universityDecan.AddToSubordinates(educator6);
+
+            ShowEducatorsInfo(kinderGartenHeadMaster);
+            ShowEducatorsInfo(schoolHeadMaster);
+            ShowEducatorsInfo(universityDecan);
+        }
+    }
+}
